c13adfe2a21e1b394eca14581a7bac1f642a2cf1
김동현 <ddhyun93@gmail.com>
2021-06-16 17:22:25 +0900

    DEPLOY 1.0.0

--- app/main.py
+++ app/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from view import channel_search
 
 app = FastAPI()
+
+origins = [
+    'http://globlin.io',
+    'https://globlin.io',
+    'https://dev.report.globlin.io',
+    'http://dev.report.globlin.io',
+    'http://report.globlin.io',
+    'https://report.globlin.io',
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'https://scheduler.globlin.io',
+    'http://scheduler.globlin.io',
+    'https://dev-scheduler.globlin.io',
+    'http://dev-scheduler.globlin.io',
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*", ],
+    allow_headers=["*", "Authorization"],
+)
+
 
 app.include_router(channel_search.router)
--- app/view/channel_search.py
+++ app/view/channel_search.py
@@ -9,18 +9,18 @@ router = APIRouter(
     tags=["channel_searcher"]
 )
 
+
 @router.get("/")
 async def index():
     return {"hello": "world"}
 
 
 @router.post("/influencer/search", response_model=List[schema.Influencer])
-async def search_influencer(params: schema.InfluencerSearcher):
+def search_influencer(params: schema.InfluencerSearcher):
     searcher = ChannelSearcher(params=params)
     searcher.search_channel_list()
     searcher.search_video_for_channel_id_list()
     searcher.search_channel_detail()
-    searcher.insert_influencer_to_db()
     try:
         result = searcher.get_channel_details_list()
     except TypeError:
